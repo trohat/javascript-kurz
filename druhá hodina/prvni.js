761660aa85e330544cd8351d5fbe73846f7f70cc
Trohat <petr.sed@centrum.cz>
2022-12-01 12:51:07 +0100

    Vstup a výstup

---
+++ druhá hodina/prvni.js
@@ -0,0 +1,42 @@
+console.log("funguju");
+
+console.log(5 + 5);
+
+console.log(6 + 7);
+
+console.log(9 - 3);
+
+
+// alert("tohle je strašný");
+
+// tohle je jednořádkový komentář
+/* tohle je
+víceřádkový komentář */
+// alert(5 + 3);
+
+let zvire = "slon";
+console.log(zvire);
+
+console.log("zvire");
+
+let vek = prompt("Kolik ti je let?");
+
+console.log("To je hezké, že ti je " + vek + " let.");
+
+let cislo1;
+let cislo2;
+let vysledek;
+cislo1 = prompt("Jdem počítat.\nZadej první číslo.");
+cislo2 = prompt("Zadej druhé číslo");
+
+cislo1 = Number(cislo1);
+cislo2 = Number(cislo2);
+
+vysledek = cislo1 + cislo2;
+
+console.log("Výsledek je " + vysledek + ".");
+
+
+
+
+
