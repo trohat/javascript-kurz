6236f8a0617881889278f36de7fc33f7fa6e9893
Trohat <petr.sed@centrum.cz>
2023-01-05 19:39:07 +0100

    Funkce, stringy a matematika

---
+++ 08 osmá hodina/kod.js
@@ -0,0 +1,30 @@
+function nicNedela() {}
+
+const pritomniStudenti = ["Tomáš", "Jana", "Petr"];
+
+pritomniStudenti.push("Martin", "Michal", "Jirka", "Hanka");
+
+pritomniStudenti.indexOf("Martin");
+pritomniStudenti.indexOf("Martin", 4);
+
+pritomniStudenti.includes("Martin");
+pritomniStudenti.includes("Martin", 4);
+
+pritomniStudenti.lastIndexOf("Martin");
+
+let pismenka = "abcdefghijkl";
+
+pismenka.length;
+pismenka.indexOf("c");
+pismenka.includes("cdef");
+
+"       Petr       ".trim();
+"SLON SEŽRAL VEVERKU".toLowerCase();
+
+let pozdrav = "Ahoj";
+pozdrav.toUpperCase();
+
+"slon a veverka jsou kamarádi".startsWith("veverka");
+"slon".padStart(10, "x");
+
+
